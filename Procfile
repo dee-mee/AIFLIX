d9b9bf7d4f3118de5982e6aa7530fe2bffc05dfa
dee-mee <derekmuriuki2@gmail.com>
2025-07-06 13:52:51 +0300

    Update WSGI configuration for port binding and static files

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: poetry run python manage.py migrate && poetry run gunicorn aiflix.wsgi:application --log-file - --bind 0.0.0.0:$PORT --workers 3
+web: poetry run python manage.py migrate && poetry run gunicorn aiflix.wsgi:application --log-file - --bind 0.0.0.0:$PORT --workers 3 --timeout 120
--- aiflix/wsgi.py
+++ aiflix/wsgi.py
@@ -10,14 +10,21 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+from decouple import config
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aiflix.settings')
 
 # Initialize Django application
 application = get_wsgi_application()
 
-# Wrap with WhiteNoise for static files
+# Configure WhiteNoise for static files
 application = WhiteNoise(application)
 
-# Add static files configuration
+# Add static files directories
 application.add_files(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static'), prefix='static/')
+application.add_files(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static_new'), prefix='static_new/')
+
+# Configure port binding
+PORT = config('PORT', default='8000')
+if os.environ.get('RENDER'):
+    PORT = config('PORT', default='10000')
